a6014398d57d953ddcc51a4169512e0685feb73d
Shishir Timalsina <revgames7@gmail.com>
2022-05-31 02:37:16 +1000

    some edits to postdetails

--- components/Layout.js
+++ components/Layout.js
@@ -22,6 +22,7 @@ export default function Layout({children}) {
             <div className='page-content'
             
             >
+                
                 {children}
             </div>
            
