c2121c7bb24cc7e9cf908853944b641557a58227
Shishir Timalsina <revgames7@gmail.com>
2023-06-06 14:39:24 +1000

    new styles for page width and colors

--- pages/blogposts/[slug].js
+++ pages/blogposts/[slug].js
@@ -3,9 +3,10 @@ import {createClient} from 'contentful'
 import Image from 'next/image'
 import styles from '../../styles/Slug.module.scss'
 import {documentToReactComponents} from '@contentful/rich-text-react-renderer'
-import {AiOutlineInstagram} from 'react-icons/ai'
-import {AiOutlineFacebook} from 'react-icons/ai'
-import {AiOutlineTwitter} from 'react-icons/ai'
+import { BLOCKS, INLINES } from "@contentful/rich-text-types";
+import {FaInstagram} from 'react-icons/fa'
+import {FaFacebook} from 'react-icons/fa'
+import {FaTwitter} from 'react-icons/fa'
 import Head from 'next/head'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
@@ -53,6 +54,56 @@ export async function getStaticProps({params}) {
   
 }
 
+const renderOptions = {
+  renderNode: {
+    [INLINES.EMBEDDED_ENTRY]: (node, children) => {
+      // target the contentType of the EMBEDDED_ENTRY to display as you need
+      if (node.data.target.sys.contentType.sys.id === "blogPost") {
+        return (
+          <a href={`/blog/${node.data.target.fields.slug}`}>            {node.data.target.fields.title}
+          </a>
+        );
+      }
+    },
+    [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
+      // target the contentType of the EMBEDDED_ENTRY to display as you need
+      if (node.data.target.sys.contentType.sys.id === "codeBlock") {
+        return (
+          <pre>
+            <code>{node.data.target.fields.code}</code>
+          </pre>
+        );
+      }
+
+      if (node.data.target.sys.contentType.sys.id === "videoEmbed") {
+        return (
+          <iframe
+            src={node.data.target.fields.embedUrl}
+            height="100%"
+            width="100%"
+
+            title={node.data.target.fields.title}
+            allowFullScreen={true}
+          />
+        );
+      }
+    },
+
+    [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
+      // render the EMBEDDED_ASSET as you need
+      return (
+        <img
+          src={`https://${node.data.target.fields.file.url}`}
+          // height={node.data.target.fields.file.details.image.height}
+          // width={node.data.target.fields.file.details.image.width}
+          width={'100%'}
+          alt={node.data.target.fields.description}
+        />
+      );
+    },
+  },
+};
+
 export default function BlogDetails({blogPost}) {
   
 
@@ -84,24 +135,24 @@ export default function BlogDetails({blogPost}) {
           <div className={styles.postdetails}>
             <div className={styles.blogauthor}>
                 <span>Shishir Timalsina</span><br/>
-                Full Stack Web and Software developer
+                Web and Software developer
             </div>
             
             <div className={styles.socials}>
 
-              <a className={styles.ig} href='https://instagram.com/shishir.codes' target="_blank" rel="noopener noreferrer">
-                <AiOutlineInstagram size={40}/>
+              <a className={styles.ig} href='https://instagram.com/sisir.dev' target="_blank" rel="noopener noreferrer">
+                <FaInstagram size={40}/>
               </a>
 
               &nbsp;&nbsp;
               &nbsp;&nbsp;
 
-              <a className={styles.fb} href='https://facebook.com/iamshishir7' target="_blank" rel="noopener noreferrer"><AiOutlineFacebook size={40}/></a>
+              <a className={styles.fb} href='https://facebook.com/iamshishir7' target="_blank" rel="noopener noreferrer"><FaFacebook size={40}/></a>
               
               &nbsp;&nbsp;
               &nbsp;&nbsp;
 
-              <a className={styles.tw} href='https://twitter.com/iamshishir7' target="_blank" rel="noopener noreferrer"><AiOutlineTwitter size={40}/></a>
+              <a className={styles.tw} href='https://twitter.com/iamshishir7' target="_blank" rel="noopener noreferrer"><FaTwitter size={40}/></a>
             </div>
           </div>
           <div className={styles.date}><span>Published on <br/>{day}</span> - {dateonly}</div>
@@ -118,7 +169,7 @@ export default function BlogDetails({blogPost}) {
         </div>
         
         <div className={styles.content}>
-            <div>{documentToReactComponents(content)}</div>
+            <div>{documentToReactComponents(content, renderOptions)}</div>
 
             
             <div className={styles.tag}>
