d3333542126638697c8c0736bc274f85ca3625bd
Shishir Timalsina <revgames7@gmail.com>
2023-06-06 16:28:28 +1000

    fixed img error

--- pages/blogposts/[slug].js
+++ pages/blogposts/[slug].js
@@ -92,12 +92,13 @@ const renderOptions = {
     [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
       // render the EMBEDDED_ASSET as you need
       return (
-        <Image
+        <img
           src={`https://${node.data.target.fields.file.url}`}
           // height={node.data.target.fields.file.details.image.height}
           // width={node.data.target.fields.file.details.image.width}
           width={'100%'}
           alt={node.data.target.fields.description}
+          layout='fill'
         />
       );
     },
