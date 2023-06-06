efc9c237f162063138a01c2ea5fd567c47489496
Shishir Timalsina <revgames7@gmail.com>
2023-06-06 16:31:10 +1000

    fixed url error

--- pages/blogposts/[slug].js
+++ pages/blogposts/[slug].js
@@ -129,7 +129,7 @@ const renderOptions = {
               )
           )
       } else {
-        return <a href={`node.data.uri`} target="_blank" rel="noreferrer">{node.content[0].value}</a>
+        return <a href={node.data.uri} target="_blank" rel="noreferrer">{node.content[0].value}</a>
       }
     },
   },
