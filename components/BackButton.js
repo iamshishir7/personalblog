3c073044cb481e534315ff53e76cc84d28c0af58
Shishir Timalsina <revgames7@gmail.com>
2023-07-08 16:10:44 +1000

    removed cosole log

--- components/BackButton.js
+++ components/BackButton.js
@@ -1,18 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import {IoChevronBack} from 'react-icons/io5'
-import styles from '../styles/BackButton.module.scss'
-import Link from 'next/link'
-import {  AnimatePresence, motion } from "framer-motion"
+import React, { useEffect, useState } from "react";
+import { IoChevronBack } from "react-icons/io5";
+import styles from "../styles/BackButton.module.scss";
+import Link from "next/link";
+import { AnimatePresence, motion } from "framer-motion";
 
 const BackButton = () => {
-
   const [showElement, setShowElement] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollY = window.scrollY;
 
-      console.log(scrollY)
       if (scrollY > 620) {
         setShowElement(true);
       }
@@ -21,30 +19,30 @@ const BackButton = () => {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
-  }, [])
+  }, []);
 
   return (
     <AnimatePresence>
-    { showElement &&
-    <motion.div
-    initial={{opacity:0}}
-    animate={{opacity:1}}
-    exit={{opacity:0}}
-    transition={{ease:"easeIn", delay:0.5}}
-    >
-    <Link href={'/'} className={styles.container}>
-        <IoChevronBack/>
-        <div>Back</div>
-    </Link>
-    </motion.div>
-    }
+      {showElement && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ ease: "easeIn", delay: 0.5 }}
+        >
+          <Link href={"/"} className={styles.container}>
+            <IoChevronBack />
+            <div>Back</div>
+          </Link>
+        </motion.div>
+      )}
     </AnimatePresence>
-  )
-}
+  );
+};
 
 export default BackButton;
